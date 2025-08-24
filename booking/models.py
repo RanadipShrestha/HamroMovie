c9096746fd145ff24f560fb4fc2cb929d464e0d3
RanadipShrestha <158831555+RanadipShrestha@users.noreply.github.com>
2025-08-24 12:18:06 +0545

    Fix: Moved secrets to .env and updated settings.py

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 # Create your models here.
+
+class Hall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    column = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Show(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True,blank=True)
+    image = models.ImageField(upload_to="images/",null=True)
+    description = models.TextField(null=True)
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    hall = models.ForeignKey(Hall,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.start_time}"
+    
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug=slugify(self.name)
+        super().save(*args,**kwargs)
